526b2dc58adb141c66aed10fc85e0d5884d6dcde
Simona Winnekes <simona.winnekes@googlemail.com>
2019-11-01 09:37:47 +0100

    discount feature

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import store from "./store";
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './store';
 
-import { Route } from "react-router-dom";
+import { Route } from 'react-router-dom';
 
-import { Provider } from "react-redux";
-import { Container } from "react-bootstrap";
-import WishListContainer from "./components/WishListContainer";
-import Navigation from "./components/Navigation";
-import CategoryListContainer from "./components/CategoryListContainer";
-import ProductsListContainer from "./components/ProductsListContainer";
-import ShoppingCartContainer from "./components/ShoppingCartContainer";
+import { Provider } from 'react-redux';
+import { Container } from 'react-bootstrap';
+
+import Navigation from './components/Navigation';
+import CategoryListContainer from './components/CategoryListContainer';
+import ProductsListContainer from './components/ProductsListContainer';
+import ShoppingCartContainer from './components/ShoppingCartContainer';
+import DiscountNotificationContainer from './components/DiscountNotificationContainer';
 function App() {
-	return (
-		<Provider store={store}>
-			<Navigation />
-			<Container>
-				<Route exact path="/" component={CategoryListContainer}></Route>
-				<Route exact path="/" component={ProductsListContainer}></Route>
-				<Route exact path="/cart" component={ShoppingCartContainer}></Route>
-				<Route exact path="/wishlist" component={WishListContainer}></Route>
-			</Container>
-		</Provider>
-	);
+    return (
+        <Provider store={store}>
+            <Navigation />
+            <DiscountNotificationContainer />
+            <Container>
+                <Route exact path="/" component={CategoryListContainer}></Route>
+                <Route exact path="/" component={ProductsListContainer}></Route>
+                <Route
+                    exact
+                    path="/cart"
+                    component={ShoppingCartContainer}
+                ></Route>
+            </Container>
+        </Provider>
+    );
 }
 
 export default App;
